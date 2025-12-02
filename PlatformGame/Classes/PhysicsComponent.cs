d7cb6176e8d6d43d9d13a95479fabb15a254d78a
HaiSam29 <s127667@ap.be>
2025-12-02 20:21:06 +0100

    Add Class PhysicsComponent

---
+++ PlatformGame/Classes/PhysicsComponent.cs
@@ -0,0 +1,33 @@
+﻿using PlatformGame.Interfaces.Character;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatformGame.Classes
+{
+    public class PhysicsComponent: IPhysicsComponent
+    {
+        private readonly float _gravity;
+
+        public Vector2 Velocity { get; set; }
+
+        public PhysicsComponent(float gravity = 800f)
+        {
+            _gravity = gravity;
+            Velocity = Vector2.Zero;
+        }
+
+        public void ApplyGravity(float deltaTime)
+        {
+            Velocity = new Vector2(Velocity.X, Velocity.Y + _gravity * deltaTime);
+        }
+
+        public void ApplyVelocity(ref Vector2 position, float deltaTime)
+        {
+            position += Velocity * deltaTime;
+        }
+    }
+}
