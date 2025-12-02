ab7f8967f300d5ecef7add1bca0e9238c88e176e
HaiSam29 <s127667@ap.be>
2025-12-02 19:43:38 +0100

    Add CharacterState enum

---
+++ PlatformGame/Enums/CharacterState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatformGame.Enums
+{
+    public enum CharacterState
+    {
+        Idle,
+        Running,
+        Jumping,
+        Falling,
+        Attacking
+    }
+}
