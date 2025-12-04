f9494dde93ecc15d545cfdcd3bd9d6fba4330910
HaiSam29 <s127667@ap.be>
2025-12-04 16:41:17 +0100

    Add Class AnimationConfig

---
+++ PlatformGame/Classes/Character/AnimationConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatformGame.Classes.Character
+{
+    public class AnimationConfig
+    {
+        public Texture2D Texture { get; set; }
+        public int FrameCount { get; set; }
+        public float FrameDuration { get; set; } // Duration of each frame in seconds
+
+        public AnimationConfig(Texture2D texture, int framecount, float frameDuration= 0.1f)
+        {
+            Texture = texture;
+            FrameCount = framecount;
+            FrameDuration = frameDuration;
+        }
+    }
+}
